b2e96e67e104b095da9506f36a81e1fae07da23c
mikeathers <athers_05@hotmail.co.uk>
2016-01-31 00:17:41 +0000

    w

--- js/mainScript.js
+++ js/mainScript.js
@@ -7,3 +7,15 @@ $('.slides').slick({
     touchMove: true
         
       });
+
+
+// When we click on the LI
+$("ul.nav li").click(function(){
+  // If this isn't already active
+  if (!$(this).hasClass("active")) {
+    // Remove the class from anything that is active
+    $("li.active").removeClass("active");
+    // And make this active
+    $(this).addClass("active");
+  }
+});
